727b6875e88a59e08fc854f5149ebb338e1bc6a6
Tian Zhou <yanxiangtianji@gmail.com>
2023-01-09 03:26:47 -0500

    fix: add flaten layer into poc model 1

--- model/poc1.py
+++ model/poc1.py
@@ -4,8 +4,8 @@ import torch.nn as nn
 from . import util
 import layer
 
-# Model: conv 3x3,5 -> relu -> conv 3x3,10 -> relu -> fc 10
-# Shape: 1x10x10 -> 5x8x8  -> 10x6x6 -> 10
+# Model: conv 3x3,5 -> relu -> conv 3x3,10 -> relu -> flatten -> fc 10
+# Shape: 1x10x10 -> 5x8x8  -> 10x6x6 -> 360 -> 10
 
 ishape = (1, 10, 10)
 Poc1Model = nn.Sequential(
@@ -13,6 +13,7 @@ Poc1Model = nn.Sequential(
     nn.ReLU(),
     nn.Conv2d(5, 10, 3),
     nn.ReLU(),
+    nn.Flatten(),
     nn.Linear(360, 10)
 )
 
