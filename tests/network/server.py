43693f36b83dc430d6e0b2849ae5a931e35314e0
Tian Zhou <yanxiangtianji@gmail.com>
2023-01-11 05:53:24 -0500

    chore: remove unnecessary code

--- tests/network/server.py
+++ tests/network/server.py
@@ -80,59 +80,6 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
         conn = self.request
         addr = self.client_address
         func(conn, addr)
-        return 
-        # 1. strings
-        data = self.request.recv(BF_SZ)
-        #print("{} wrote:".format(self.client_address[0]))
-        print(f"1, receive from [{self.client_address}]: {data}")
-        # just send back the same data, but upper-cased
-        self.request.sendall(data.upper())
-        # 2. numpy array
-        data = self.request.recv(BF_SZ)
-        d = np.frombuffer(data, dtype=np.float32) # result of frombuffer is read-only when the data is a bytes string
-        print(f"2.1, receive from [{self.client_address}]: {d}")
-        d = d * 2
-        self.request.sendall(d.tobytes())
-        data = self.request.recv(BF_SZ)
-        d = np.frombuffer(data, dtype=np.int16)
-        print(f"2.2, receive from [{self.client_address}]: {d}")
-        d = d + 1
-        self.request.sendall(d.tobytes())
-        # 3. AES ciphertext
-        data = self.request.recv(BF_SZ)
-        d = cipher.decryptor()
-        r = d.update(data) + d.finalize()
-        print(f"3, receive from [{self.client_address}]: {r}")
-        self.request.sendall(r)
-        # 4. big data (multiple packets)
-        buffer = []
-        chunck = self.request.recv(BF_SZ)
-        n, len_type_str, num_shape = struct.unpack('!iii', chunck[:12])
-        header_len = 12 + len_type_str + num_shape*4
-        type_str = chunck[12:12+len_type_str].decode()
-        shape = struct.unpack('!'+'i'*num_shape, chunck[12+len_type_str:12+len_type_str+num_shape*4])
-        print(f"4.0, receive from [{self.client_address}]: n={n}, type={type_str}, shape={shape}")
-        if len(chunck) > header_len:
-            print(f"4.x, receive from [{self.client_address}]: {len(buffer)+1}-th len={len(chunck)-header_len} left={n}")
-            buffer.append(chunck[header_len:])
-            n -= len(chunck) - header_len
-        while n > 0:
-            chunck = self.request.recv(BF_SZ)
-            print(f"4.x, receive from [{self.client_address}]: {len(buffer)+1}-th len={len(chunck)} left={n}")
-            n -= len(chunck)
-            buffer.append(chunck)
-        data = b''.join(buffer)
-        d = np.frombuffer(data, dtype=type_str).reshape(shape)
-        s = d.sum()
-        print(f"4, receive from [{self.client_address}]: {len(data)} {d.shape} {s}")
-        self.request.sendall(struct.pack('!f', s)+struct.pack('!'+'i'*len(d.shape), *d.shape))
-        # 5. torch tensor
-        data = self.request.recv(BF_SZ)
-        d = torch.load(io.BytesIO(data))
-        s = d.sum()
-        print(f"5, receive from [{self.client_address}]: {len(data)} {d.shape} {s}")
-        self.request.sendall(struct.pack('!f', s))
-        print('close connection')
         
         
 host, port = "localhost", 8000
