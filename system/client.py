8b2595786f6418bfd0fe3afb9f762db9f9339344
Tian Zhou <yanxiangtianji@gmail.com>
2023-01-11 05:04:10 -0500

    chore: add buffer function for client and server

--- system/client.py
+++ system/client.py
@@ -1,13 +1,25 @@
 import socket
+import torch
+import torch.nn as nn
 
+from model import util
+import layer
 
 class Client():
-    def __init__(self, host, port, model):
-        self.host = host
-        self.port = port
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.connect((host, port))
-        self.socket.sendall(b"Hello, world")
+    def __init__(self, socket: socket.socket, model: nn.Module, inshape: tuple):
+        self.socket = socket
         # model
         self.model = model
+        self.inshape = inshape
+        # layers
+        self.layers = util.make_client_model(socket, model, inshape)
         
+    def offline(self):
+        for lyr in self.layers:
+            lyr.offline()
+            
+    def online(self, data: torch.Tensor) -> torch.Tensor:
+        for lyr in self.layers:
+            data = lyr.online(data)
+        return data
+
--- system/server.py
+++ system/server.py
@@ -1,15 +1,41 @@
 import socket
+import torch
+import torch.nn as nn
+
+from model import util
+import layer
 
 class Server():
-    def __init__(self, host, port, model) -> None:
-        self.host = host
-        self.port = port
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.bind((host, port))
-        self.socket.listen()
-        conn, addr = self.socket.accept()
-        if conn:
-            print("Connected by", addr)
-            data = conn.recv(1024)
+    def __init__(self, socket: socket.socket, model: nn.Module, inshape: tuple):
+        self.socket = socket
         # model
         self.model = model
+        self.inshape = inshape
+        # layers
+        self.layers, self.to_buffer = util.make_server_model(socket, model, inshape)
+        # data bufer
+        self.data = {}
+        
+    def offline(self):
+        buffer = {}
+        for i, lyr in enumerate(self.layers):
+            if i in self.to_buffer:
+                if isinstance(lyr, layer.shortcut.ShortCutServer):
+                    t = lyr.offline(buffer[i + lyr.other_offset])
+                else:
+                    t = lyr.offline()
+                buffer[i] = t
+            else:
+                lyr.offline()
+            
+    def online(self):
+        buffer = {}
+        for i, lyr in enumerate(self.layers):
+            if i in self.to_buffer:
+                if isinstance(lyr, layer.shortcut.ShortCutServer):
+                    t = lyr.online(buffer[i + lyr.other_offset])
+                else:
+                    t = lyr.online()
+                buffer[i] = t
+            lyr.online()
+        
