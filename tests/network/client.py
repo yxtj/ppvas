d7ff5ac167a48993af5b0be39a09941605d97ada
Tian Zhou <yanxiangtianji@gmail.com>
2023-01-12 03:27:34 -0500

    test: add HE ciphertext transferring

--- tests/network/client.py
+++ tests/network/client.py
@@ -4,6 +4,7 @@ from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 import struct
 import io
 import torch
+from Pyfhel import Pyfhel, PyCtxt
 
 BF_SZ = 4096
 host, port = "localhost", 8000
@@ -11,6 +12,15 @@ host, port = "localhost", 8000
 key = b'abcdefghijklmnop'
 iv = b'ABCDEFGHIJKLMNOP'
 cipher = Cipher(algorithms.AES(key), modes.CBC(iv))
+
+def receive_big(conn, n):
+    buffer = []
+    while n > 0:
+        t = conn.recv(min(BF_SZ, n))
+        n -= len(t)
+        buffer.append(t)
+    data = b''.join(buffer)
+    return data
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((host, port))
@@ -59,4 +69,35 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     data = s.recv(BF_SZ)
     sum2 = struct.unpack('!f', data[:4])[0]
     print(f"5, receive from [{host}]: {sum2}. The difference is {sum1-sum2}")
+    # 6.1 HE ciphertext
+    he = Pyfhel()
+    he.contextGen(scheme='ckks', n=2**13, scale=2**30, qi_sizes=[30]*5)
+    he.keyGen()
+    b_ctx = he.to_bytes_context()
+    s.sendall(struct.pack('!i', len(b_ctx))+b_ctx)
+    print(f"6.1, send to [{host}]: context {len(b_ctx)} bytes")
+    x = np.random.random(10)
+    cx = he.encryptFrac(x)
+    bcx = cx.to_bytes()
+    y = np.random.random(10)
+    cy = he.encryptFrac(y)
+    bcy = cy.to_bytes()
+    s.sendall(struct.pack('!i', len(bcx))+bcx)
+    s.sendall(struct.pack('!i', len(bcy))+bcy)
+    print(f"6.1, send to [{host}]: x {x.sum()}, y {y.sum()}")
+    data = s.recv(4)
+    n, = struct.unpack('!i', data)
+    data = receive_big(s, n)
+    cxy = PyCtxt(pyfhel=he, bytestring=data)
+    xy = cxy.decrypt()[:10]
+    diff = np.abs(x+y-xy).sum()
+    print(f"6.1, receive from [{host}]: x+y {xy.sum()}. The difference is {diff}")
+    # 6.2 HE keys
+    b_pk = he.to_bytes_public_key()
+    s.sendall(struct.pack('!i', len(b_pk))+b_pk)
+    b_sk = he.to_bytes_secret_key()
+    s.sendall(struct.pack('!i', len(b_sk))+b_sk)
+    print(f"6.2, send to [{host}]: pk {len(b_pk)}, sk {len(b_sk)} bytes")
+    
+    
     
--- tests/network/server.py
+++ tests/network/server.py
@@ -6,12 +6,27 @@ from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 import struct
 import io
 import torch
+from Pyfhel import Pyfhel, PyCtxt
 
 BF_SZ = 4096
 
 key = b'abcdefghijklmnop'
 iv = b'ABCDEFGHIJKLMNOP'
 cipher = Cipher(algorithms.AES(key), modes.CBC(iv))
+
+def receive_int(conn):
+    data = conn.recv(4)
+    n, = struct.unpack('!i', data)
+    return n
+
+def receive_big(conn, n):
+    buffer = []
+    while n > 0:
+        t = conn.recv(min(BF_SZ, n))
+        n -= len(t)
+        buffer.append(t)
+    data = b''.join(buffer)
+    return data
 
 def func(conn, addr):
     # 1. strings
@@ -65,6 +80,34 @@ def func(conn, addr):
     s = d.sum()
     print(f"5, receive from [{addr}]: {len(data)} {d.shape} {s}")
     conn.sendall(struct.pack('!f', s))
+    # 6.1 HE ciphertext
+    he = Pyfhel()
+    n = receive_int(conn)
+    data = receive_big(conn, n)
+    he.from_bytes_context(data)
+    n = receive_int(conn)
+    data1 = receive_big(conn, n)
+    cx = PyCtxt(pyfhel=he, bytestring=data1)
+    n = receive_int(conn)
+    data2 = receive_big(conn, n)
+    cy = PyCtxt(pyfhel=he, bytestring=data2)
+    print(f"6.1, receive from [{addr}]: x {len(data1)} y {len(data2)} bytes")
+    c = cx + cy
+    data = c.to_bytes()
+    conn.sendall(struct.pack('!i', len(data))+data)
+    print(f"6.1, send to [{addr}]: x+y {len(data)} bytes")
+    # 6.2 HE keys
+    n = receive_int(conn)
+    b_pk = receive_big(conn, n)
+    he.from_bytes_public_key(b_pk)
+    n = receive_int(conn)
+    b_sk = receive_big(conn, n)
+    he.from_bytes_secret_key(b_sk)
+    print(f"6.2, receive from [{addr}]: pk {len(b_pk)} sk {len(b_sk)} bytes")
+    x = he.decryptFrac(cx)
+    y = he.decryptFrac(cy)
+    print(f"6.2, receive from [{addr}]: x {x.sum()}, y {y.sum()}, x+y {(x+y).sum()}")
+    
     print('close connection')
 
 class MyTCPHandler(socketserver.BaseRequestHandler):
@@ -90,22 +133,24 @@ def method1():
         server.serve_forever()
         
 def method2():
-    while True:
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.bind((host, port))
-            s.listen()
-            conn, addr = s.accept()
-            with conn:
-                func(conn, addr)
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((host, port))
+        s.listen()
+        print('start server')
+        while True:
+                conn, addr = s.accept()
+                with conn:
+                    func(conn, addr)
 
 def method3():
+    s = socket.create_server((host, port), family=socket.AF_INET)
+    print('start server')
     while True:
-        s = socket.create_server((host, port), family=socket.AF_INET)
         conn, addr = s.accept()
         with conn:
             func(conn, addr)
     
 
-# method1()
+method1()
 # method2()
-method3()
+# method3()
