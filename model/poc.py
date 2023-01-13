8303b49cc9188de95b5595eb2c1744fa96e5712b
Tian Zhou <yanxiangtianji@gmail.com>
2023-01-12 23:06:08 -0500

    test: generalize poc test function

---
+++ model/poc.py
@@ -0,0 +1,44 @@
+# import torch
+import torch.nn as nn
+from torch_extension.shortcut import ShortCut
+
+# Model 1:
+# Shape: 1x10x10 -> 5x8x8  -> 10x6x6 -> 360 -> 10
+
+Poc1Inshape = (1, 10, 10)
+Poc1Model = nn.Sequential(
+    nn.Conv2d(1, 5, 3),
+    nn.ReLU(),
+    nn.Conv2d(5, 10, 3),
+    nn.ReLU(),
+    nn.Flatten(),
+    nn.Linear(360, 10),
+    # nn.Softmax()
+)
+
+# Model 2:
+# Shape: 1x32x32 -conv-> 5x30x30 -max-> 5x10x10 -conv-> 10x8x8 -max-> 10x3x3 -> 90 -> 10
+
+Poc2Inshape = (1, 32, 32)
+Poc2Model = nn.Sequential(
+    nn.Conv2d(1, 5, 3),
+    nn.ReLU(),
+    nn.MaxPool2d(3, 3),
+    nn.Conv2d(5, 10, 3),
+    nn.ReLU(),
+    nn.MaxPool2d(3, 3),
+    nn.Flatten(),
+    nn.Linear(360, 10),
+)
+
+# Model 3:
+# Shape: 20 -fc-> 10 -relu-> 10 -shortcut(-1,-3)-> 10 -> 5
+
+Poc3Inshape = (20)
+Poc3Model = nn.Sequential(
+    nn.Linear(20, 10),
+    nn.ReLU(),
+    nn.Linear(10, 10),
+    ShortCut(-3),
+    nn.Linear(10, 5),
+)
--- tests/model/poc1.py
+++ tests/model/poc1.py
@@ -2,25 +2,37 @@ import sys
 import torch
 import socket
 
-from model import poc1
+from model import poc
 import system
 
 
 if __name__ == '__main__':
     argv = sys.argv
-    if len(argv) < 2:
-        print('Usage: python [server|client] [host=localhost] [port=8100]')
+    if len(argv) < 3:
+        print('Usage: python model server|client [host=localhost] [port=8100]')
         sys.exit(1)
-    mode = argv[1]
+    model_name = argv[1]
+    mode = argv[2]
     assert mode in ['server', 'client']
-    host = argv[2] if len(argv) >= 3 else 'localhost'
-    port = int(argv[3]) if len(argv) >= 4 else 8100
+    host = argv[3] if len(argv) > 3 else 'localhost'
+    port = int(argv[4]) if len(argv) > 4 else 8100
+    
+    # set model and inshape
+    if model_name == 'poc1':
+        model, inshape = poc.Poc1Model, poc.Poc1Inshape
+    elif model_name == 'poc2':
+        model, inshape = poc.Poc2Model, poc.Poc2Inshape
+    elif model_name == 'poc3':
+        model, inshape = poc.Poc3Model, poc.Poc3Inshape
+    else:
+        raise ValueError("Unknown model name: {}".format(model_name))
+    
     if mode == 'server':
         s = socket.create_server((host, port))
         print("Server is running on {}:{}".format(host, port))
         conn, addr = s.accept()
         print("Client connected from: {}".format(addr))
-        server = system.Server(conn, poc1.Poc1Model, poc1.ishape)
+        server = system.Server(conn, poc.Poc1Model, poc.ishape)
         print("Server is ready")
         server.offline()
         print("Server offline finished")
@@ -29,16 +41,16 @@ if __name__ == '__main__':
     else:
         s = socket.create_connection((host, port))
         print("Client is connecting to {}:{}".format(host, port))
-        client = system.Client(s, poc1.Poc1Model, poc1.ishape)
+        client = system.Client(s, poc.Poc1Model, poc.ishape)
         print("Client is ready")
         client.offline()
         print("Client offline finished")
-        inshape = (1, *poc1.ishape)
+        inshape = (1, *poc.ishape)
         data = torch.rand(inshape)
         res = client.online(data)
         print("Client online finished")
         print("System result: {}".format(res))
-        res2 = poc1.Poc1Model(data)
+        res2 = poc.Poc1Model(data)
         print("Local result: {}".format(res2))
         diff = (res - res2).abs().sum()
         print("Difference: {}".format(diff))
