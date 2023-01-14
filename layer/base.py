f213276a334c8e8c829f5994832e2db504481f0e
Tian Zhou <yanxiangtianji@gmail.com>
2023-01-14 00:16:30 -0500

    feat: adding function for statistics

--- layer/base.py
+++ layer/base.py
@@ -23,6 +23,16 @@ class Stat:
     byte_online_recv: float
     # time_online_send: float
     # time_online_recv: float
+    
+    def __add__(self, other):
+        return Stat(
+            self.time_offline + other.time_offline,
+            self.byte_offline_send + other.byte_offline_send,
+            self.byte_offline_recv + other.byte_offline_recv,
+            self.time_online + other.time_online,
+            self.byte_online_send + other.byte_online_send,
+            self.byte_online_recv + other.byte_online_recv,
+        )
 
 
 class LayerCommon():
