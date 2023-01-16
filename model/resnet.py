48c33ab6010034d17db1b35900ea8cd626d0b8e5
Tian Zhou <yanxiangtianji@gmail.com>
2023-01-16 06:02:50 -0500

    feat: update resnet implementation

---
+++ model/resnet.py
@@ -0,0 +1,86 @@
+# reference: https://github.com/akamaster/pytorch_resnet_cifar10/blob/master/resnet.py
+
+# import torch
+import torch.nn as nn
+import torch_extension as te
+
+inshape = (3, 32, 32)
+
+# 3x3 convolution
+def conv3x3(in_channels, out_channels, stride=1):
+    return nn.Conv2d(in_channels, out_channels, 3, 
+                     stride=stride, padding=1, bias=False)
+
+def build_downsample_block(in_channels, out_channels, stride=2):
+    layers = [
+        conv3x3(in_channels, out_channels, stride),
+        nn.ReLU(),
+        conv3x3(out_channels, out_channels),
+        # nn.ReLU(),
+    ]
+    if stride == 1:
+        layers.append(te.ShortCut(-4))
+    layers.append(nn.ReLU())
+    return layers
+    
+def build_identity_block(channels):
+    layers = [
+        conv3x3(channels, channels),
+        nn.ReLU(),
+        conv3x3(channels, channels),
+        te.ShortCut(-4),
+        nn.ReLU(),
+    ]
+    return layers
+
+def build_block(layer_size, in_channels, out_channels, stride):
+    layers = build_downsample_block(in_channels, out_channels, stride)
+    for i in range(layer_size-1):
+        layers.extend(build_identity_block(out_channels))
+    return layers
+
+def build_resnet(num_blocks, num_class=100, version=1):
+    # layer 0: 3x32x32 -> 16x32x32
+    layers = [ conv3x3(3, 16), nn.ReLU() ]
+    # layer 1: 16x32x32 -> 16x32x32
+    layers.extend(build_block(num_blocks[0], 16, 16, 1))
+    # layer 2: 16x32x32 -> 32x16x16
+    layers.extend(build_block(num_blocks[1], 16, 32, 2))
+    # layer 3: 32x16x16 -> 64x8x8
+    layers.extend(build_block(num_blocks[2], 32, 64, 2))
+    # pooling and fc
+    if version == 1:
+        layers.extend([
+            nn.AvgPool2d(8),
+            nn.Flatten(),
+            nn.Linear(64, num_class),
+        ])
+    elif version == 2:
+        layers.extend([
+            nn.Conv2d(64, 64, 8, 8, bias=False),
+            nn.Flatten(),
+            nn.Linear(64, num_class),
+        ])
+    elif version == 3:
+        layers.extend([
+            nn.Flatten(),
+            nn.Linear(4096, num_class),
+        ])
+    elif version == 4:
+        layers.extend([
+            nn.AvgPool2d(2),
+            nn.Flatten(),
+            nn.Linear(1024, num_class),
+        ])
+    layers.append(nn.Softmax())
+    return te.SequentialBuffer(*layers)
+
+
+def resnet20(version=1):
+    return build_resnet([3, 3, 3], 100, version)
+
+def resnet32(version=1):
+    return build_resnet([5, 5, 5], 100, version)
+
+def resnet44(version=1):
+    return build_resnet([7, 7, 7], 100, version)
