1b2e189fbf3f76e0cfe51db4f0334a38ca1c226c
Tian Zhou <yanxiangtianji@gmail.com>
2023-01-24 19:55:25 -0500

    test: add test with pretrained weights

--- tests/model/minionn.py
+++ tests/model/minionn.py
@@ -1,4 +1,5 @@
 import sys
+import torch
 from Pyfhel import Pyfhel
 
 from model import minionn
@@ -7,18 +8,21 @@ from system.runner import run_client, run_server
 if __name__ == '__main__':
     argv = sys.argv
     if len(argv) < 2:
-        print('Usage: python server|client [n=1] [host=localhost] [port=8100]')
+        print('Usage: python server|client [n=1] [weight_file] [host=localhost] [port=8100]')
         sys.exit(1)
     # model_name = argv[1]
     mode = argv[1]
     assert mode in ['server', 'client']
     n = int(argv[2]) if len(argv) > 2 else 1
-    host = argv[3] if len(argv) > 3 else 'localhost'
-    port = int(argv[4]) if len(argv) > 4 else 8100
+    wfile = argv[3] if len(argv) > 3 else None
+    host = argv[4] if len(argv) > 4 else 'localhost'
+    port = int(argv[5]) if len(argv) > 5 else 8100
     
     # set model and inshape
     inshape = minionn.inshape
     model = minionn.build()
+    if wfile is not None:
+        model.load_state_dict(torch.load(wfile))
     
     if mode == 'server':
         run_server(host, port, model, inshape, n)
@@ -26,4 +30,4 @@ if __name__ == '__main__':
         he = Pyfhel()
         he.contextGen(scheme='ckks', n=2**13, scale=2**30, qi_sizes=[30]*5)
         he.keyGen()
-        run_client(host, port, model, inshape, he, None, n)
+        run_client(host, port, model, inshape, he, None, n, True)
--- tests/model/resnet.py
+++ tests/model/resnet.py
@@ -1,7 +1,5 @@
 import sys
-import time
 import torch
-import socket
 from Pyfhel import Pyfhel
 
 from model import resnet
@@ -10,23 +8,26 @@ from system.runner import run_client, run_server
 if __name__ == '__main__':
     argv = sys.argv
     if len(argv) < 2:
-        print('Usage: python server|client [ver=3] [n=1] [host=localhost] [port=8100]')
+        print('Usage: python server|client [ver=3] [n=1] [weight_file=None] [host=localhost] [port=8100]')
         sys.exit(1)
     # model_name = argv[1]
     mode = argv[1]
     assert mode in ['server', 'client']
     ver = int(argv[2]) if len(argv) > 2 else 3
     n = int(argv[3]) if len(argv) > 3 else 1
-    host = argv[4] if len(argv) > 4 else 'localhost'
-    port = int(argv[5]) if len(argv) > 5 else 8100
+    wfile = argv[4] if len(argv) > 4 else None
+    host = argv[5] if len(argv) > 5 else 'localhost'
+    port = int(argv[6]) if len(argv) > 6 else 8100
     
     # set model and inshape
     inshape = resnet.inshape
     model = resnet.resnet32(ver)
+    if wfile is not None:
+        model.load_state_dict(torch.load(wfile))
     
     if mode == 'server':
         run_server(host, port, model, inshape, n)
     else:
         he = Pyfhel()
         he.contextGen(scheme='ckks', n=2**13, scale=2**30, qi_sizes=[30]*5)
-        run_client(host, port, model, inshape, he, None, n)
+        run_client(host, port, model, inshape, he, None, n, True)
