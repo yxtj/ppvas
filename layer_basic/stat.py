f7e639674e089911e1530d673cbb42bb5dc1dab3
Tian Zhou <yanxiangtianji@gmail.com>
2023-01-28 00:44:55 -0500

    feat: better presentation of statistics

--- layer_basic/stat.py
+++ layer_basic/stat.py
@@ -22,4 +22,28 @@ class Stat:
             self.byte_online_send + other.byte_online_send,
             self.byte_online_recv + other.byte_online_recv,
         )
+    
+    def show(self, promot:str='', n:int=1):
+        def _show_time_(t):
+            if t < 1e-3:
+                return f"{t*1e6:.3f} us"
+            elif t < 1:
+                return f"{t*1e3:.3f} ms"
+            else:
+                return f"{t:.3f} s"
+            
+        def _show_byte_(b):
+            if b < 1e6:
+                return f"{b/1e3:.3f} kB"
+            elif b < 1e9:
+                return f"{b/1e6:.3f} MB"
+            else:
+                return f"{b/1e9:.3f} GB"
+        
+        print("{}: Offline time {}, send {}, recv {}; Online time {}, send {}, recv {}".format(
+            promot, _show_time_(self.time_offline),
+            _show_byte_(self.byte_offline_send), _show_byte_(self.byte_offline_recv),
+            _show_time_(self.time_online/n),
+            _show_byte_(self.byte_online_send/n), _show_byte_(self.byte_online_recv/n),
+        ))
 
