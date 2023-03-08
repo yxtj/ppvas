7f1fd79b66bb0b8c98c3516d748d0afbef471a0c
Tian Zhou <yanxiangtianji@gmail.com>
2023-03-08 03:23:01 -0500

    chore: remove assertion from setting.py

--- setting.py
+++ setting.py
@@ -8,7 +8,6 @@ USE_HE = os.environ.get('FASENET_USE_HE', '0').lower() in ['1', 'true', 'yes']
 print("USE_HE:", USE_HE)
 
 # which protocol to use
-PROTOCOL = os.environ.get('FASENET_PROTOCOL', 'dual')
-assert PROTOCOL in ['dual', 'smp'], "Invalid protocol: {}".format(PROTOCOL)
+PROTOCOL = os.environ.get('FASENET_PROTOCOL', 'shuffle')
 
 print("PROTOCOL:", PROTOCOL)
