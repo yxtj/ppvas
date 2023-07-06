597516e32aed34674ef02c83bca0d2bc162ab17a
Tian Zhou <yanxiangtianji@gmail.com>
2023-07-06 02:24:19 -0400

    chore: remove unnecessary

--- layer_basic/__init__.py
+++ layer_basic/__init__.py
@@ -1,3 +1,2 @@
 from .stat import Stat
-from .sshare import *
 from .layercommon import LayerCommon
--- layer_basic/layercommon.py
+++ layer_basic/layercommon.py
@@ -1,13 +1,10 @@
 from socket import socket
-from typing import Union
-import numpy as np
-import torch
+# from typing import Union
+# import numpy as np
+# import torch
 from Pyfhel import Pyfhel
 
-import comm
 from .stat import Stat
-
-from setting import USE_HE
 
 
 class LayerCommon():
@@ -18,30 +15,3 @@ class LayerCommon():
         self.he = he
         self.stat = Stat()
     
-    def send_plain(self, data:torch.Tensor) -> None:
-        self.stat.byte_online_send += comm.send_torch(self.socket, data)
-    
-    def recv_plain(self) -> torch.Tensor:
-        data, nbyte = comm.recv_torch(self.socket)
-        self.stat.byte_online_recv += nbyte
-        return data
-
-    def send_he(self, data:Union[np.ndarray, torch.Tensor]) -> None:
-        if USE_HE:
-            # actual send with HE
-            assert isinstance(data, np.ndarray)
-            self.stat.byte_offline_send += comm.send_he_matrix(self.socket, data, self.he)
-        else:
-            # simulate with plain
-            assert isinstance(data, torch.Tensor)
-            self.stat.byte_offline_send += comm.send_torch(self.socket, data)
-    
-    def recv_he(self) -> np.ndarray:
-        if USE_HE:
-            # actual send with HE
-            data, nbytes = comm.recv_he_matrix(self.socket, self.he)
-        else:
-            # simulate with plain
-            data, nbytes = comm.recv_torch(self.socket)
-        self.stat.byte_offline_recv += nbytes
-        return data
